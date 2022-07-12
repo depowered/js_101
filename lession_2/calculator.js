a88603bbfa7932ce8a12e4e108522878ca365ebc
Devin Power <dpwr16@gmail.com>
2022-07-12 15:11:33 -0500

    extract messages to config file

--- lession_2/calculator.js
+++ lession_2/calculator.js
@@ -1,3 +1,5 @@
+const MESSAGES = require("./calculator_messages.json");
+
 const readline = require('readline-sync');
 
 function prompt(message) {
@@ -8,52 +10,62 @@ function invalidNumber(number) {
   return number.trim() === '' || Number.isNaN(Number(number));
 }
 
-prompt('Welcome to Calculator!');
+while (true) {
+  prompt(MESSAGES['welcome']);
 
-// Ask the user for the first number.
-prompt('What is the first number?');
-let number1 = readline.question();
+  // Ask the user for the first number.
+  prompt(MESSAGES['firstNumber']);
+  let number1 = readline.question();
 
-while (invalidNumber(number1)) {
-  prompt('Input provided is not a valid number. Please try again.');
-  number1 = readline.question();
-}
+  while (invalidNumber(number1)) {
+    prompt(MESSAGES['invalidNumber']);
+    number1 = readline.question();
+  }
 
-// Ask the user for the second number.
-prompt('What is the second number?');
-let number2 = readline.question();
+  // Ask the user for the second number.
+  prompt(MESSAGES['secondNumber']);
+  let number2 = readline.question();
 
-while (invalidNumber(number2)) {
-  prompt('Input provided is not a valid number. Please try again.');
-  number2 = readline.question();
-}
+  while (invalidNumber(number2)) {
+    prompt(MESSAGES['invalidNumber']);
+    number2 = readline.question();
+  }
 
-// Ask the user for an operation to perform.
-prompt('What operation would you like to preform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
+  // Ask the user for an operation to perform.
+  prompt(MESSAGES['operationsLine1']);
+  prompt(MESSAGES['operationsLine2']);
+  let operation = readline.question();
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Input provided is not 1, 2, 3, or 4. Please try again.');
-  operation = readline.question();
-}
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(MESSAGES['invalidOperation']);
+    operation = readline.question();
+  }
 
-// Perform the operation on the two numbers.
-let output;
+  // Perform the operation on the two numbers.
+  let output;
 
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  // Print the result to the terminal.
+  prompt(`The result is: ${output}`);
+
+  // Ask the user if they would like restart the calculator
+  prompt(MESSAGES['repeat']);
+  let repeat = readline.question();
+  if (repeat === "" || repeat.toLowerCase()[0] !== 'y') {
     break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
+  }
 }
-
-// Print the result to the terminal.
-prompt(`The result is: ${output}`);
