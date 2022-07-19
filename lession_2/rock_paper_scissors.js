f314403ada457e9eb125f8be408ef64d90605927
Devin Power <dpwr16@gmail.com>
2022-07-19 15:32:12 -0500

    more refactoring

--- lession_2/rock_paper_scissors.js
+++ lession_2/rock_paper_scissors.js
@@ -2,78 +2,72 @@ const readline = require('readline-sync');
 
 const RULES = {
   rock: {
-    fullName: 'rock',
-    abbreviation: 'r',
+    validInputs: ['rock', 'r'],
     beats: 'lizard'
   },
   paper: {
-    fullName: 'paper',
-    abbreviation: 'p',
+    validInputs: ['paper', 'p'],
     beats: 'rock'
   },
   scissors: {
-    fullName: 'scissors',
-    abbreviation: 'sc',
+    validInputs: ['scissors', 'sc'],
     beats: 'paper'
   },
   lizard: {
-    fullName: 'lizard',
-    abbreviation: 'l',
+    validInputs: ['lizard', 'l'],
     beats: 'spock'
   },
   spock: {
-    fullName: 'spock',
-    abbreviation: 'sp',
+    validInputs: ['spock', 'sp'],
     beats: 'scissors'
   }
 };
 
-const CHOICE_ABBREVIATIONS = {
-  r: "rock",
-  p: "paper",
-  sc: "scissors",
-  l: "lizard",
-  sp: "spock"
-};
-
+const VALID_INPUTS = getAllValidInputs();
 const VALID_CHOICES = Object.keys(RULES);
-const VALID_ABBREVIATIONS = Object.keys(CHOICE_ABBREVIATIONS);
 
 function displayChoiceOptions() {
-  prompt('Choose one:');
+  displayMessage('Choose one:');
   for (let option of Object.keys(RULES)) {
-    prompt(`  ${RULES[option]['fullName']} (${RULES[option]['abbreviation']})`);
+    let validInputs = RULES[option]['validInputs'];
+    displayMessage(`  ${validInputs[0]} (${validInputs[1]})`);
   }
+}
+
+function getAllValidInputs() {
+  let validInputs = [];
+  for (let option of Object.keys(RULES)) {
+    validInputs += (RULES[option]['validInputs']);
+  }
+  return validInputs;
 }
 
 function getChoiceFromInputString(inputString) {
-  if (VALID_CHOICES.includes(inputString)) {
-    return inputString;
-  } else if (VALID_ABBREVIATIONS.includes(inputString)) {
-    return CHOICE_ABBREVIATIONS[inputString];
-  } else {
-    return null;
+  for (let option of Object.keys(RULES)) {
+    if (RULES[option]['validInputs'].includes(inputString)) {
+      return option;
+    }
   }
+  return null;
 }
 
 function isValidChoice(inputString) {
-  return (VALID_CHOICES.includes(inputString) ||
-    VALID_ABBREVIATIONS.includes(inputString));
+  return (VALID_INPUTS.includes(inputString));
 }
 
-function prompt(message) {
+function displayMessage(message) {
   console.log(`=> ${message}`);
 }
 
 function displayWinner(playerChoice, computerChoice) {
-  prompt(`You chose ${playerChoice}. Computer chose ${computerChoice}.`);
+  displayMessage(`You chose ${playerChoice}. Computer chose ${computerChoice}.`);
 
   if (playerChoice === computerChoice) {
-    prompt("It's a tie.");
+    displayMessage("It's a tie.");
   } else if (RULES[playerChoice]['beats'] === computerChoice) {
-    prompt('You won!');
+    displayMessage('You won!');
   } else {
-    prompt('Computer won!');
+    displayMessage('Computer won!');
   }
 }
 
@@ -82,7 +76,7 @@ while (true) {
   let input = readline.question().trim().toLocaleLowerCase();
 
   while (!isValidChoice(input)) {
-    prompt("That's not a valid choice.");
+    displayMessage("That's not a valid choice.");
     input = readline.question().trim().toLocaleLowerCase();
   }
 
@@ -93,10 +87,11 @@ while (true) {
 
   displayWinner(playerChoice, computerChoice);
 
-  prompt('Do you want to play again (y/n)?');
+  displayMessage('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
+
   while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
+    displayMessage('Please enter "y" or "n".');
     answer = readline.question().toLowerCase();
   }
 
